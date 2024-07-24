41b3d7b351532292ee67febcc790d2b0cc4337c1
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-07-24 10:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-//your JS code here. If required.
+function focusNext(currentInput) {
+    if (currentInput.value.length === 1) {
+        const nextInput = currentInput.nextElementSibling;
+        if (nextInput) {
+            nextInput.focus();
+        }
+    }
+}
 
-let element = document.getElementsByClassName("code");
-
-for(let i=0; i<element.length; i++){
-	element[i].addEventListener("keyup", (event) =>{
-		
-		let currentElement = event.target
-		// -----
-		if(event.key === "Backspace"){
-			let prevElement = currentElement.previousElementSibling;
-			if(prevElement){
-				prevElement.focus();
-			}
-			return;
-		}
-			let code = event.key.charCodeAt(0);
-		if(code>= 48 && code <=57){
-			let nextElemet = currentElement.nextElementSibling;
-			if(nextElemet){
-				nextElemet.focus();
-			}
-			
-		}
-		else{
-			event.target.value="";
-		}
-	})
+function handleBackspace(currentInput, event) {
+    if (currentInput.value === '' && event.key === 'Backspace') {
+        const previousInput = currentInput.previousElementSibling;
+        if (previousInput) {
+            previousInput.focus();
+        }
+    }
 }
