5e5a6e35d6a2ab153de6d33e302caafc7cd3fc29
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-07-24 10:53:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
 //your JS code here. If required.
-let inputs=document.getElementsByTagName('input');
-console.log(inputs);
-// window.addEventListener('load',()=>{
-//     inputs[0].addEventListener('focus',targets);
-// })
-inputs[0].addEventListener('change',()=>{
-    inputs[1].focus();
-    if(inputs[0].value===""){
-        inputs[0].focus();
-    }
-})
-inputs[1].addEventListener('change',()=>{
-    inputs[2].focus();
-    if(inputs[1].value===""){
-        inputs[0].focus();
-    }
-})
-inputs[2].addEventListener('change',()=>{
-    inputs[3].focus();
-    if(inputs[2].value===""){
-        inputs[1].focus();
-    }
-})
-inputs[3].addEventListener('change',()=>{
-    inputs[4].focus();
-    if(inputs[3].value===""){
-        inputs[2].focus();
-    }
-})
-inputs[4].addEventListener('change',()=>{
-    inputs[5].focus();
-    if(inputs[4].value===""){
-        inputs[3].focus();
-    }
-})
-inputs[5].addEventListener('change',()=>{
-    if(inputs[5].value===""){
-        inputs[4].focus();
-    }
-})
+
+let element = document.getElementsByClassName("code");
+
+for(let i=0; i<element.length; i++){
+	element[i].addEventListener("keyup", (event) =>{
+		
+		let currentElement = event.target
+		// -----
+		if(event.key === "Backspace"){
+			let prevElement = currentElement.previousElementSibling;
+			if(prevElement){
+				prevElement.focus();
+			}
+			return;
+		}
+			let code = event.key.charCodeAt(0);
+		if(code>= 48 && code <=57){
+			let nextElemet = currentElement.nextElementSibling;
+			if(nextElemet){
+				nextElemet.focus();
+			}
+			
+		}
+		else{
+			event.target.value="";
+		}
+	})
+}
